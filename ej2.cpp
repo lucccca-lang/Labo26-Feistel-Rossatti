4d757f988323ce1c3508b47bba395c410f9d3c04
lucccca-lang <alumno27.rossatti.lucca.santino@ipm.edu.ar>
2026-03-11 09:49:41 -0300

    ms

--- ej2.cpp
+++ ej2.cpp
@@ -4,7 +4,66 @@ using namespace std;
 #include <vector>
 
 struct Productos {
-    
+    string nombre;
+    int codigoBarra;
+    string marca;
+    int precio;
+    bool fechaVencimiento;
+
+};
+
+const int F = 4;
+const int C = 3;
+
+void CargarGondola(Productos gondola [F] [C]){
+Productos prod;
+
+prod.nombre = "fideos";
+prod.codigoBarra = 132443234 ;
+prod.marca = "luchetti";
+prod.precio = 1000;
+prod.fechaVencimiento = false;
+gondola[0] [0] = prod;
+
+prod.nombre = "chocolate";
+prod.codigoBarra = 65437345;
+prod.marca = "Milka";
+prod.precio = 550;
+prod.fechaVencimiento = false;
+gondola[0] [1] = prod;
+
+prod.nombre = "queso";
+prod.codigoBarra = 19283743;
+prod.marca = "casancream";
+prod.precio = 850;
+prod.fechaVencimiento = true;
+gondola[0] [2] = prod;
+
+prod.nombre = "leche";
+prod.codigoBarra = 14527689;
+prod.marca = "laSerenisima";
+prod.precio = 700;
+prod.fechaVencimiento = true; 
+gondola[0] [3] = prod;
+
+prod.nombre = "huevo";
+prod.codigoBarra = 42537824;
+prod.marca = "monumental";
+prod.precio = 1000;
+prod.fechaVencimiento = true; 
+gondola[1] [0] = prod;
+
+prod.nombre = "refresco";
+prod.codigoBarra = 14653628;
+prod.marca = "cocaCola";
+prod.precio = 1500;
+prod.fechaVencimiento = true; 
+gondola[1] [1] = prod;
+
+
 
 
 }
+
+
+
